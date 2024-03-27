4f5a464fbd219cb1c8c8052ba33d42b7ec5ce7b9
Delmitch <107454012+mitchinnn@users.noreply.github.com>
2024-03-27 20:04:27 +0800

    db.js

--- db.js
+++ db.js
@@ -2,12 +2,13 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'chin123',
+  password: 'micin2',
   host: 'localhost',
   port: 5432, // default Postgres port
-  database: 'exercise7'
+  database: 'postgres'
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
 };
+
